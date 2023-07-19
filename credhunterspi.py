5dba3dc1e00fe90c14a60f557b5dc1bfbdd71e8c
websitedsigns <websitedsigns@gmail.com>
2023-07-19 15:40:36 +0100

    modernise the gui
    modernised gui, add coaster, manufacturer, park and country input with ability to filter via one of these choices

--- credhunterspi.py
+++ credhunterspi.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
-from ttkthemes import ThemedStyle
 
-def add_coaster(*args):
+# Function to handle adding a coaster
+def add_coaster():
     coaster = coaster_entry.get()
     manufacturer = manufacturer_entry.get()
     theme_park = theme_park_entry.get()
@@ -11,11 +11,11 @@ def add_coaster(*args):
 
     if coaster and manufacturer and theme_park and country:
         # Check for duplicate entries
-        if coaster in coaster_count:
+        if coaster in coaster_data:
             if messagebox.askyesno("Duplicate Entry", "This coaster already exists. Do you want to add it again?"):
-                coaster_count[coaster] += 1
+                coaster_data[coaster] = {'manufacturer': manufacturer, 'theme_park': theme_park, 'country': country}
         else:
-            coaster_count[coaster] = 1
+            coaster_data[coaster] = {'manufacturer': manufacturer, 'theme_park': theme_park, 'country': country}
 
         # Update coaster listbox and running total
         update_coaster_listbox()
@@ -27,79 +27,118 @@ def add_coaster(*args):
         theme_park_entry.delete(0, tk.END)
         country_entry.delete(0, tk.END)
 
+    else:
+        messagebox.showerror("Error", "Please fill in all fields.")
+
+# Function to update the coaster listbox
 def update_coaster_listbox():
     coaster_listbox.delete(0, tk.END)
-    for item, count in coaster_count.items():
-        coaster_listbox.insert(tk.END, item)
+    for coaster in coaster_data.keys():
+        coaster_listbox.insert(tk.END, coaster)
 
+# Function to update the running total
 def update_running_total():
-    total_count = sum(coaster_count.values())
+    total_count = len(coaster_data)
     total_label.config(text=f"Total Coasters: {total_count}")
 
+# Function to filter coasters based on park, country, or manufacturer
+def filter_coasters():
+    selected_filter = filter_combobox.get()
+    search_query = search_entry.get().lower()
+
+    # Clear the listbox
+    coaster_listbox.delete(0, tk.END)
+
+    # Filter and display the matching coasters
+    for coaster, data in coaster_data.items():
+        if selected_filter == "Park" and data['theme_park'].lower() == search_query:
+            coaster_listbox.insert(tk.END, coaster)
+        elif selected_filter == "Country" and data['country'].lower() == search_query:
+            coaster_listbox.insert(tk.END, coaster)
+        elif selected_filter == "Manufacturer" and data['manufacturer'].lower() == search_query:
+            coaster_listbox.insert(tk.END, coaster)
+
+# Create the main window
 root = tk.Tk()
-root.geometry("800x600")
 root.title("CredHunters")
+root.geometry("800x600")
+root.resizable(False, False)
 
-style = ThemedStyle(root)
-style.set_theme("plastik")
+# Create a style for the GUI elements
+style = ttk.Style()
+style.theme_use("clam")
 
-# Title label
-title_label = ttk.Label(root, text="CredHunters", font=("Arial", 24, "bold"), foreground="white", background="darkblue")
+# Configure style options
+style.configure("TLabel", foreground="black", background="#F0F0F0", font=("Arial", 12))
+style.configure("TEntry", fieldbackground="#FDFDFD", font=("Arial", 12))
+style.configure("TButton", background="#4E96B0", foreground="white", font=("Arial", 12))
+style.configure("TCombobox", font=("Arial", 12))
+
+# Create the title label
+title_label = ttk.Label(root, text="Cred Hunters", font=("Arial", 18, "bold"))
 title_label.pack(pady=20)
 
-# Coaster input frame
-input_frame = tk.Frame(root, padx=10, pady=10)
-input_frame.pack()
+# Create the main frame
+main_frame = ttk.Frame(root, padding=20)
+main_frame.pack(fill=tk.BOTH, expand=True)
 
-# Coaster input fields
+# Create the input frame
+input_frame = ttk.Frame(main_frame)
+input_frame.pack(pady=20)
+
+# Create the coaster input fields
 coaster_label = ttk.Label(input_frame, text="Coaster:")
-coaster_label.grid(row=0, column=0, sticky=tk.W)
+coaster_label.grid(row=0, column=0, sticky=tk.E, padx=5, pady=5)
 coaster_entry = ttk.Entry(input_frame, width=30)
-coaster_entry.grid(row=0, column=1, padx=5)
+coaster_entry.grid(row=0, column=1, padx=5, pady=5)
 
 manufacturer_label = ttk.Label(input_frame, text="Manufacturer:")
-manufacturer_label.grid(row=1, column=0, sticky=tk.W)
+manufacturer_label.grid(row=1, column=0, sticky=tk.E, padx=5, pady=5)
 manufacturer_entry = ttk.Entry(input_frame, width=30)
-manufacturer_entry.grid(row=1, column=1, padx=5)
+manufacturer_entry.grid(row=1, column=1, padx=5, pady=5)
 
 theme_park_label = ttk.Label(input_frame, text="Theme Park:")
-theme_park_label.grid(row=2, column=0, sticky=tk.W)
+theme_park_label.grid(row=2, column=0, sticky=tk.E, padx=5, pady=5)
 theme_park_entry = ttk.Entry(input_frame, width=30)
-theme_park_entry.grid(row=2, column=1, padx=5)
+theme_park_entry.grid(row=2, column=1, padx=5, pady=5)
 
 country_label = ttk.Label(input_frame, text="Country:")
-country_label.grid(row=3, column=0, sticky=tk.W)
+country_label.grid(row=3, column=0, sticky=tk.E, padx=5, pady=5)
 country_entry = ttk.Entry(input_frame, width=30)
-country_entry.grid(row=3, column=1, padx=5)
-country_entry.bind("<Return>", add_coaster)
+country_entry.grid(row=3, column=1, padx=5, pady=5)
+country_entry.bind("<Return>", lambda event: add_coaster())
 
-# Button frame
-button_frame = tk.Frame(root, pady=10)
-button_frame.pack()
+# Create the Add Coaster button
+add_button = ttk.Button(main_frame, text="Add Coaster", command=add_coaster)
+add_button.pack(pady=10)
 
-# Add Coaster button
-add_button = ttk.Button(button_frame, text="Add Coaster", command=add_coaster)
-add_button.pack(side=tk.LEFT, padx=5)
+# Create the filter frame
+filter_frame = ttk.Frame(main_frame)
+filter_frame.pack(pady=20)
 
-# Remove Coaster button
-remove_button = ttk.Button(button_frame, text="Remove Coaster", command=remove_coaster)
-remove_button.pack(side=tk.LEFT, padx=5)
+# Create the filter combobox
+filter_combobox = ttk.Combobox(filter_frame, values=["Park", "Country", "Manufacturer"], state="readonly")
+filter_combobox.current(0)
+filter_combobox.pack(side=tk.LEFT, padx=5)
+filter_combobox.configure(style="TCombobox")
 
-# Analyze Data button
-analyze_button = ttk.Button(button_frame, text="Analyze Data", command=analyze_data)
-analyze_button.pack(side=tk.LEFT, padx=5)
+# Create the search entry
+search_entry = ttk.Entry(filter_frame, width=20)
+search_entry.pack(side=tk.LEFT, padx=5)
+search_entry.configure(style="TEntry")
+search_entry.bind("<Return>", lambda event: filter_coasters())
 
-# Generate Statistics button
-statistics_button = ttk.Button(button_frame, text="Generate Statistics", command=generate_statistics)
-statistics_button.pack(side=tk.LEFT, padx=5)
+# Create the search button
+search_button = ttk.Button(filter_frame, text="Search", command=filter_coasters)
+search_button.pack(side=tk.LEFT)
+search_button.configure(style="TButton")
 
-# Coaster listbox
-coaster_count = {}
-coaster_listbox = tk.Listbox(root, width=40)
+# Create the coaster listbox
+coaster_data = {}
+coaster_listbox = tk.Listbox(main_frame, width=40)
 coaster_listbox.pack(pady=10)
 
-# Running total label
-total_label = ttk.Label(root, text="Total Coasters: 0")
+# Create the running total label
+total_label = ttk.Label(main_frame, text="Total Coasters: 0", font=("Arial", 14, "bold"))
 total_label.pack()
 
-root.mainloop()
